0abd086b8920277f29bd7cdb34072bf56e4f8a15
Villela <0262659@students.dadeschools.net>
2019-02-15 12:03:10 -0500

    Finished car game

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Variables
+//VARIABLES
 let car = {
 	make: 'Tesla',
 	model: 'Roadster',
@@ -8,26 +8,63 @@ let car = {
 
 	driveToWork() {
 		alert(`Old Mileage: ${this.mileage}`);
-		this.mileage = this.mileage + 10;
+		this.mileage += 10;
 		alert(`New Mileage: ${this.mileage}`);
-	};
+	},
 
+	driveAroundUSA() {
+		alert(`Old Mileage: ${this.mileage}`);
+		this.mileage += 7000;
+		alert(`New Mileage: ${this.mileage}`);
+		alert('Car needs a tune up!');
+		this.isWorking = false;
+	},
+
+	getTuneUp() {
+		alert('Car is fixed and ready to go!');
+		this.isWorking = true;
+	},
+
+	honk() {
+		alert('Beep! Beep!');
+	},
 }
 
-//Functions
+//FUNCTIONS
 //logs all our cars current stats to the console
-const rewriteStats() => {
+const rewriteStats=() => {
+	console.log(`Make: ${car.make}`);
+	console.log(`Make: ${car.model}`);
+	console.log(`Make: ${car.color}`);
+	console.log(`Make: ${car.mileage}`);
+	console.log(`Make: ${car.isWorking}`);
+	console.log('-----------------------');
 
 }
 
-//Main Process
+//MAIN PROCESS
 //captures keyboard input depending on the letter pressed it will "call" (execute) at different functions 
- const document.onkeyup(event)=>{
+ document.onkeyup = (event)=>{
  	//captures the key press. converts it to lowercase and sends it to the variable 
- 	let letter = string.fromCarCode(events.keyCode).toLowerCase();
+ 	let letter = String.fromCharCode(event.keyCode).toLowerCase();
 
- 	if (letter === 'd'){
+ 	if (letter === 'd') {
  		car.driveToWork();
+ 		rewriteStats();
+ 	}
+
+ 	if (letter === 'r') {
+ 		car.driveAroundUSA();
+ 		rewriteStats();
+ 	}
+
+ 	if (letter === 't') {
+ 		car.getTuneUp();
+ 		rewriteStats();
+ 	}
+
+ 	if (letter === 'h') {
+ 		car.honk();
  		rewriteStats();
  	}
  }
